a6d42ac2b20b25060584f5bb36b2e2cfce399131
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-28 07:26:04 -0400

    typedef & resolvers

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -2,7 +2,13 @@ const { Book, User } = require('../models');
 
 const resolvers = {
   Query: {
+    books: async () => {
+      return await Book.find({});
+    },
 
+    users: async () => {
+      return await User.find({});
+    },
   }
 };
 
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,7 +1,32 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+  # like models. check lecture 1 36:00
+  type Book {
+    _id: String!
+    authors: String
+    description: String!
+    image: String
+    link: String
+    title: String!
+
+  }
+
+  type User {
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [String]
+  }
+
+  type Query {
+    books: [Book]
+    users: [User]
+  }
+
+  type Mutation {
+
+  }
 `;
 
 module.exports = typeDefs;
